f0af9853e5245da09243eeda3a0456619f385786
gyeongisung <gisung.gyeon@gmail.com>
2023-06-27 20:24:23 +0900

    feat: Footer, Intro 작업 완료

--- src/App.js
+++ src/App.js
@@ -6,18 +6,24 @@ import Diet from "./pages/Diet";
 import Health from "./pages/Health";
 import MyPage from "./pages/MyPage";
 import NotFound from "./pages/NotFound";
+import Schedule from "./pages/Schedule";
+import List from "./pages/List";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/intro" element={<Intro />}></Route>
+        <Route path="/" element={<Intro />}></Route>
         <Route path="/main" element={<Main />}></Route>
         <Route path="/diet" element={<Diet />}></Route>
         <Route path="/health" element={<Health />}></Route>
+        <Route path="/schedule" element={<Schedule />}></Route>
+        <Route path="/list" element={<List />}></Route>
         <Route path="/mypage" element={<MyPage />}></Route>
         <Route path="/notfound" element={<NotFound />}></Route>
       </Routes>
+      <Footer />
     </div>
   );
 }
--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Intro = () => {
-  return <div>Intro</div>;
+  return (
+    <div>
+      <Link to="/main">
+        <img
+          src="images/intro.jpg"
+          alt="intro"
+          style={{
+            width: "100%",
+            height: "900px",
+          }}
+        />
+      </Link>
+    </div>
+  );
 };
 
 export default Intro;
