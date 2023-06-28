424c14c4d3d57dbb840478ede3f10f7678f65e32
dev.cappp <dev.cappp@gmail.com>
2023-06-28 20:25:37 +0900

    feat: Diet page 작업 중

--- src/pages/Diet.js
+++ src/pages/Diet.js
@@ -1,125 +1,179 @@
 import React, { useState } from "react";
-import { PlusOutlined } from "@ant-design/icons";
-import { Upload, message, Select } from "antd";
-// import { DietUpload } from "../style/DietCss";
-import { Row } from "../style/DietCss";
+import { DietUpload, mealTime, Row, calorie } from "../style/DietCss";
 
-const getBase64 = (img, callback) => {
-  const reader = new FileReader();
-  reader.addEventListener("load", () => callback(reader.result));
-  reader.readAsDataURL(img);
-};
-const beforeUpload = file => {
-  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
-  if (!isJpgOrPng) {
-    message.error("You can only upload JPG/PNG file!");
+import { PlusOutlined, InfoCircleOutlined } from "@ant-design/icons";
+import { Modal, Upload, Radio, Select, Input, Button, Form } from "antd";
+const { TextArea } = Input;
+// 파일 미리보기
+const getBase64 = file =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
+
+const normFile = e => {
+  if (Array.isArray(e)) {
+    return e;
   }
-  const isLt2M = file.size / 1024 / 1024 < 2;
-  console.log(isLt2M);
-  if (!isLt2M) {
-    message.error("Image must smaller than 2MB!");
-  }
-  return isJpgOrPng && isLt2M;
+  return e?.fileList;
 };
 
 const Diet = () => {
-  const [num, setNum] = useState(1);
+  // Modal 창 활성화 여부
+  const [previewOpen, setPreviewOpen] = useState(false);
+  // Modal 창에 보여줄 파일명
+  const [previewTitle, setPreviewTitle] = useState("");
+  // 선택된 이미지 미리보기
+  const [previewImage, setPreviewImage] = useState("");
+  // 모달창 닫기
+  const handleCancel = () => setPreviewOpen(false);
 
-  const handleChangeSelected = value => {
-    console.log(`selected ${value}`);
-  };
+  // 업로드할 이미지 목록 관리
+  const [fileList, setFileList] = useState([]);
 
-  const double = () => {
-    setNum(num * 2);
-  };
-  console.log(num);
-
-  const [loading, setLoading] = useState(false);
-  const [imageUrl, setImageUrl] = useState();
-  const handleChangeUpload = info => {
-    if (info.file.status === "uploading") {
-      setLoading(true);
-      return;
+  // upload 컴포넌트에서 처리
+  const handlePreview = async file => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
     }
-    if (info.file.status === "done") {
-      // Get this url from response in real world.
-      getBase64(info.file.originFileObj, url => {
-        setLoading(false);
-        setImageUrl(url);
-      });
-    }
+    setPreviewImage(file.url || file.preview);
+    setPreviewOpen(true);
+    setPreviewTitle(
+      file.name || file.url.substring(file.url.lastIndexOf("/") + 1),
+    );
   };
+  // upload 컴포넌트에서 처리
+  const handleChange = ({ fileList: newFileList }) => setFileList(newFileList);
+
+  // 업로드 버튼
   const uploadButton = (
     <div>
-      {<PlusOutlined />}
+      <PlusOutlined />
       <div
         style={{
           marginTop: 8,
         }}
       >
-        Upload 임
+        Upload
       </div>
     </div>
   );
 
-  return (
-    <>
-      <Upload
-        name="avatar"
-        listType="picture-card"
-        className="avatar-uploader"
-        showUploadList={false}
-        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-        beforeUpload={beforeUpload}
-        onChange={handleChangeUpload}
-      >
-        {imageUrl ? (
-          <img
-            src={imageUrl}
-            alt="avatar"
-            style={{
-              width: "100%",
-            }}
-          />
-        ) : (
-          uploadButton
-        )}
-      </Upload>
+  // form 관련
+  const [form] = Form.useForm();
+  const [requiredMark, setRequiredMarkType] = useState("");
+  const onRequiredTypeChange = ({ requiredMarkValue }) => {
+    setRequiredMarkType(requiredMarkValue);
+  };
 
-      {/* <DietUpload>
-        <p>+</p>
-      </DietUpload> */}
-      <Row>
-        <h2>음식목록:</h2>
-        <dropDownDiet>
-          <Select
-            defaultValue="음식을 선택해주세요."
-            style={{
-              width: 100,
-            }}
-            onChange={handleChangeSelected}
-            options={[
-              {
-                options: [
-                  {
-                    label: "샐러드",
-                    value: "샐러드",
-                  },
-                  {
-                    label: "사과",
-                    value: "사과",
-                  },
-                  {
-                    label: "밥",
-                    value: "밥",
-                  },
-                ],
-              },
-            ]}
-          />
-        </dropDownDiet>
-      </Row>
-    </>
+  return (
+    <div>
+      <div>주간달력</div>
+      <DietUpload>
+        <Form
+          labelCol={{
+            span: 4,
+          }}
+          wrapperCol={{
+            span: 14,
+          }}
+          style={{
+            maxWidth: 500,
+          }}
+          form={form}
+          layout="horizontal"
+          initialValues={{
+            requiredMarkValue: requiredMark,
+          }}
+          onValuesChange={onRequiredTypeChange}
+          requiredMark={requiredMark}
+        >
+          <Form.Item
+            label="Upload"
+            valuePropName="fileList"
+            getValueFromEvent={normFile}
+            rules={[{ required: true }]}
+            name="d"
+          >
+            <Upload
+              action="/upload.do"
+              listType="picture-card"
+              fileList={fileList}
+              onPreview={handlePreview}
+              onChange={handleChange}
+            >
+              {fileList.length >= 1 ? null : uploadButton}
+            </Upload>
+          </Form.Item>
+
+          <Modal
+            open={previewOpen}
+            title={previewTitle}
+            footer={null}
+            onCancel={handleCancel}
+          >
+            <img
+              alt="example"
+              style={{
+                width: "100%",
+              }}
+              src={previewImage}
+            />
+          </Modal>
+
+          <Form.Item
+            label="식사시간"
+            name="mealtime"
+            rules={[{ required: true }]}
+          >
+            <Radio.Group>
+              <Radio.Button value="blackfirst">아침</Radio.Button>
+              <Radio.Button value="lunch">점심</Radio.Button>
+              <Radio.Button value="dinner">저녁</Radio.Button>
+            </Radio.Group>
+          </Form.Item>
+
+          <Form.Item label="식단선택" name="a" rules={[{ required: true }]}>
+            <Select
+              options={[
+                {
+                  label: "샐러드",
+                  value: "샐러드",
+                },
+                {
+                  label: "사과",
+                  value: "사과",
+                },
+                {
+                  label: "밥",
+                  value: "밥",
+                },
+              ]}
+              style={{
+                width: 200,
+              }}
+            ></Select>
+          </Form.Item>
+
+          <Form.Item label="칼로리" name="calorie" rules={[{ required: true }]}>
+            <Input />
+          </Form.Item>
+
+          <Form.Item label="메모(option)" name="memo">
+            <TextArea rows={4} />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Save
+            </Button>
+            <Button>Cancel</Button>
+          </Form.Item>
+        </Form>
+      </DietUpload>
+    </div>
   );
 };
 
--- src/style/DietCss.js
+++ src/style/DietCss.js
@@ -1,28 +1,39 @@
 import styled from "@emotion/styled";
 
 export const DietUpload = styled.div`
-  position: relative;
-  margin-top: 68px;
-  width: 150px;
-  height: 120px;
-  background: #fff;
-  border: 2px dotted #000;
-  border-radius: 20px;
-  p {
-    position: absolute;
-    padding-left: 53px;
-    font-size: 80px;
-    text-align: center;
-  }
-`;
-export const Row = styled.div`
-  display: flex;
+  margin: 30px auto;
+
+  /* margin-top: 150px; */
+  /* margin-left: 160px; */
 `;
 
-export const dropDownDiet = styled.div`
-  margin-top: 387px;
-  margin-left: 112px;
-  .ant-select {
-    width: 50px;
+export const mealTimeRow = styled.div`
+  display: flex;
+  margin-left: 100px;
+  margin-top: 20px;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  margin-left: 100px;
+  margin-top: 20px;
+`;
+export const Calorie = styled.div`
+  display: flex;
+  margin-left: 20px;
+  h2 {
+    margin-left: 10px;
+  }
+  input {
+    margin-left: 10px;
+    border: 1px solid #d9d9d9;
+    border-radius: 6px;
   }
 `;
+// export const dropDownDiet = styled.div`
+//   margin-top: 387px;
+//   margin-left: 112px;
+//   .ant-select {
+//     width: 1px;
+//   }
+// `;
