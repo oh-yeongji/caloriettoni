777478ea00561e9ebaf2584a3691f87328849589
gyeongisung <gisung.gyeon@gmail.com>
2023-06-28 18:45:49 +0900

    feat: Schedule 작업 중

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import "react-calendar/dist/Calendar.css";
+import {
+  CalendarWrap,
+  CalendarMain,
+  CalendarSpace,
+} from "../style/ScheduleCss";
+import moment from "moment";
 
 const Schedule = () => {
-  return <div>Schedule</div>;
+  const [value, onChange] = useState(new Date());
+
+  return (
+    <CalendarWrap>
+      <h2>Calorittoni</h2>
+      <div>
+        <form>
+          <input type="text"></input>
+        </form>
+      </div>
+      <CalendarSpace></CalendarSpace>
+      <CalendarMain
+        onChange={onChange}
+        value={value}
+        calendarType="US"
+        formatDay={(locale, date) => moment(date).format("D")}
+      />
+    </CalendarWrap>
+  );
 };
 
 export default Schedule;
