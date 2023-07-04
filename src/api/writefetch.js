1a9186c6134286b645a629b66845cad8ad04a263
dev.cappp <dev.cappp@gmail.com>
2023-07-04 21:52:56 +0900

    feat:fetch 작업중

--- src/api/writefetch.js
+++ src/api/writefetch.js
@@ -6,17 +6,26 @@ import axios from "axios";
 
 //************************운동********************
 
-// 운동카테고리목록 Get 기능
-export const getHealthCate = async () => {
+//운동 사진 업로드 Get기능
+export const getHealthUpload = async () => {
   try {
-    const res = await axios.get("/api/exrec/exlist");
+    const res = await axios.get("");
     const result = res.data;
     console.log(result);
   } catch (err) {
     console.log(err);
   }
 };
-
-
+// 운동카테고리목록 Get 기능
+export const getHealthCate = async () => {
+  try {
+    const res = await axios.get("/api/exrec/exlist");
+    const result = res.data;
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 //*******************식단**************************
