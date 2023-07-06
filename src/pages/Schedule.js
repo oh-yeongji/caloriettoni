5dc1dead765359e5ceaeda4589fc57b4ae95bde7
gyeongisung <gisung.gyeon@gmail.com>
2023-07-06 21:35:10 +0900

    feat: Calendar 각 기능들 axios 연동 작업

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -1,11 +1,14 @@
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { getScheduleTextData } from "../api/schedulefetch";
 import moment from "moment";
 import { CalendarWrap, CalendarMain } from "../style/ScheduleCss";
 import "react-calendar/dist/Calendar.css";
-import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ScheduleList from "../components/ScheduleList";
 
 const Schedule = () => {
+  const [value, onChange] = useState(new Date());
+  const [scheduleText, setScheduleText] = useState([]);
   const navigate = useNavigate();
 
   const handleDateClick = date => {
@@ -13,27 +16,41 @@ const Schedule = () => {
     navigate(`/list/${dayClick}`);
   };
 
+  useEffect(() => {
+    getScheduleTextData(setScheduleText);
+  }, []);
 
-  
+  // 날짜 값을 받아야하는데..?...??????????????????????????
+  // diet,health 작성 날짜..?
+  const marks = [
+    "12-06-2023",
+    "13-06-2023",
+    "15-06-2023",
+    "01-07-2023",
+    "02-07-2023",
+    "03-07-2023",
+  ];
+
   return (
     <CalendarWrap>
       <h2>Calorittoni</h2>
       <div>
-        <form>
-          <textarea
-            name="textarea"
-            placeholder="본인의 목표롤 설정하고 실천해 보세요!"
-          />
-          <button type="submit">
-            <FontAwesomeIcon icon={faPenToSquare} />
-          </button>
-        </form>
+        <ScheduleList
+          scheduleText={scheduleText}
+          setScheduleText={setScheduleText}
+        />
       </div>
       <CalendarMain
-        value={new Date()}
+        onChange={onChange}
+        value={value}
         calendarType="US"
         formatDay={(locale, date) => moment(date).format("D")}
         onClickDay={date => handleDateClick(date)}
+        tileClassName={({ date, view }) => {
+          if (marks.find(x => x === moment(date).format("DD-MM-YYYY"))) {
+            return "highlight";
+          }
+        }}
       />
     </CalendarWrap>
   );
